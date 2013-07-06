243074ec1eb5ae333207373f09399b4ecd042f6e
Bhargava Chinthirla <bchinthirla@gmail.com>
2013-07-06 12:19:16 +0000

    Read in index.html and send out value in web.js

--- web.js
+++ web.js
@@ -3,7 +3,13 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  // Read in buffer from index.html
+  var buffer = fs.readFileSync("index.html");
+  // convert buffer returned to string
+  var line = buffer.toString();
+  // send out line instead of previous value
+  response.send(line);
+  //response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
